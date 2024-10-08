4870a5929d199c56ac83dcc7c0840b4acfa4da95
oripovfakhriddin <fakhriddinoripov0731@gmail.com>
2024-10-08 09:53:54 +0500

    login icon added

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -14,6 +14,7 @@ import AccountIcon from '../../public/icons/account'
 import OrderIcon from '../../public/icons/order'
 import HamburgerIcon from '../../public/icons/hamburger'
 import CloseIcon from '../../public/icons/close'
+import LoginIcon from '../../public/icons/login'
 
 const menuItems = [
   {
@@ -25,15 +26,18 @@ const menuItems = [
     label: "Mahsulot",
     icon: <ProductIcon />,
     href: "/products"
-  },{
+  },
+  {
     label: "Saralangan",
     icon: <FavouriteIcon />,
     href: "/favourite"
-  },{
+  },
+  {
     label: "Kategoriya",
     icon: <CategoryIcon />,
     href: "/category"
-  },{
+  },
+  {
     label: "Savat",
     icon: <CartIcon />,
     href: "/cart"
@@ -42,12 +46,8 @@ const menuItems = [
     label: "Buyurtma",
     icon: <OrderIcon />,
     href: "/orders"
-  },
-  {
-    label: "Profil",
-    icon: <AccountIcon />,
-    href: "/account"
   }
+  
 ]
 
 const Navbar = () => {
@@ -55,20 +55,33 @@ const Navbar = () => {
   const [isNavbar, setIsNavbar] = useState(false)
 
   return (
-    <header className="flex items-center px-4 md:px-12 py-2 justify-between fixed top-0 w-full z-50 shadow bg-white dark:bg-boxdark-1">
-      <Link href={"/"} >
-        <Image priority src={ParfumIcon} alt={"Logotip"}  className='w-10 h-10' />
+    <header className="flex items-center px-4 py-2 md:px-8 justify-between fixed top-0 w-full z-50 shadow bg-white dark:bg-boxdark-1">
+      <Link href={"/"} className='flex items-center gap-1' >
+        <Image priority src={ParfumIcon} alt={"Logotip"}  className='w-10 h-10' /> 
+        <p className='text-primary text-xl'>PARFUM</p>
       </Link>
-      <div className='flex gap-4 items-center'>
+      <div className='flex gap-1 md:gap-4 items-center'>
       <nav >
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
           <button onClick={()=>{setIsNavbar(!isNavbar)}} type="button" className="inline-flex items-center p-1 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 border-gray-200  outline-none ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
-              { !isNavbar ? <HamburgerIcon />: <CloseIcon />}
+              { !isNavbar ? <HamburgerIcon /> : <CloseIcon />}
           </button>
           <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-           <ul className="flex flex-col font-medium md:space-x-8  md:flex-row">
+            <ul className="flex flex-col font-medium md:space-x-2 lg:space-x-4  md:flex-row">
               {menuItems.map((item, index)=> {return <li key={index}><NavLink  item={item} /></li>})}
-           </ul>
+              <li>
+                <Link href={"/auth/login"} className={`flex items-center flex-col text-primary`}>
+                  <div><LoginIcon /></div>
+                  <p>Kirish</p>
+                </Link>
+              </li>
+              <li>
+                <Link href={"/account"} className={`flex items-center flex-col`}>
+                  <div><AccountIcon /></div>
+                  <p>Profil</p>
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
